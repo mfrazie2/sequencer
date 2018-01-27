c8be7bd87e16f39c7d1bc1fbee2c7c10fd833c2a
mfrazie2 <michael.s.frazier@gmail.com>
2018-01-27 14:46:26 -0700

    feat:Add sounds and mock sequence
    * Refactor eventListeners to be added/removed as needed
    * Add listener for space bar keyup
    * Fix calculation of BPM to MS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,31 +4,30 @@
       isNewSequence = true,
       isRunning = false;
 
-  
-  document.getElementById('start').addEventListener('click', function() { 
-    !isRunning ? startInterval(100) : null; 
-  });
-  document.getElementById('stop').addEventListener('click', stopInterval);
+  let sequence = [
+    [1,4], [], [3], [],
+    [2,4], [], [3], [],
+    [1,4], [], [3], [],
+    [2,4], [], [1], [5,2],
+  ];
 
-  function startInterval(bpm) {
-    let interval = convertBPMToMS(bpm);
-    isRunning = true;
-    intervalTimer = setInterval(startSequencer, interval);
-  }
-  
-  function stopInterval() {
-    clearInterval(intervalTimer);
-    if (isRunning) toggleActiveStep(getElementByStepNumber(stepNumber - 1));
-    stepNumber = 1;
-    isNewSequence = true;
-    isRunning = false;
-  }
+  /*
+    // let sequence = [
+    //   [9], [9], [9], [9],
+    //   [9], [9], [9], [9],
+    //   [9], [9], [9], [9],
+    //   [9], [9], [9], [9],
+    // ];
+  */
+
+  document.addEventListener('keyup', startStopOnSpace);
+  addEventListener('start', startListener);
 
   function convertBPMToMS(bpm) {
-    return 1000 * (1 / ( bpm / 60 ));
+    return 1000 * (1 / ( bpm / 60 )) / 4;
   }
 
-  function startSequencer() {
+  function runSequencer() {
     if (stepNumber > 16) stepNumber = 1;
     let prevStepNumber;
     if (stepNumber === 1) {
@@ -39,19 +38,82 @@
 
     let currentEl = getElementByStepNumber(stepNumber);
     let previousEl = getElementByStepNumber(prevStepNumber);
-    
+
     toggleActiveStep(currentEl);
     if (!isNewSequence) toggleActiveStep(previousEl);
+
+    playStepSounds(stepNumber);
 
     stepNumber++;
     isNewSequence = false;
   }
 
-  function getElementByStepNumber(stepNumber) {
-    return document.getElementsByClassName(`button${stepNumber}`)[0];
+  function startInterval(bpm) {
+    let interval = convertBPMToMS(bpm);
+    isRunning = true;
+    intervalTimer = setInterval(runSequencer, interval);
+    runSequencer();
   }
 
-  function toggleActiveStep(element) {
-    element.classList.toggle('activeStep');
+  function stopInterval() {
+    clearInterval(intervalTimer);
+    if (isRunning) toggleActiveStep(getElementByStepNumber(stepNumber - 1));
+    stepNumber = 1;
+    isNewSequence = true;
+    isRunning = false;
   }
+
+  /* LISTENERS START */
+    function startListener() {
+      startInterval(95);
+      addEventListener('stop', stopListener);
+      removeEventListener('start', startListener);
+    }
+
+    function stopListener() {
+      stopInterval();
+      removeEventListener('stop', stopListener);
+      addEventListener('start', startListener);
+    }
+
+    function startStopOnSpace(e) {
+      if (e.keyCode === 32) {
+        if (isRunning) {
+          stopListener();
+        } else {
+          startListener();
+        }
+      }
+    }
+  /* LISTENERS END */
+
+
+  /* HELPERS START */
+    function getElementByStepNumber(stepNumber) {
+      return document.getElementsByClassName(`button${stepNumber}`)[0];
+    }
+
+    function toggleActiveStep(element) {
+      element.classList.toggle('activeStep');
+    }
+
+    function playStepSounds(stepNumber) {
+      let step = sequence[stepNumber - 1];
+      step.forEach(playSound);
+    }
+
+    function playSound(buttonNumber) {
+      let audio = document.querySelector(`audio[data-button="${buttonNumber}"]`);
+      audio.currentTime = 0;
+      audio.play();
+    }
+
+    function addEventListener(id, listener) {
+      document.getElementById(id).addEventListener('click', listener);
+    }
+
+    function removeEventListener(id, listener) {
+      document.getElementById(id).removeEventListener('click', listener);
+    }
+  /* HELPERS END */
 })()
